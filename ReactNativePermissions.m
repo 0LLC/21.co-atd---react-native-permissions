64aed97e2afe7a93a632d2890cb1e84296cb7bb3
Brandon Bayer <b@bayer.ws>
2017-03-24 09:06:48 -0500

    temp workaround for those who don't want bluetooh or events
    See yonahforst#46

--- ReactNativePermissions.m
+++ ReactNativePermissions.m
@@ -29,10 +29,8 @@
 #endif
 
 #import "RNPLocation.h"
-#import "RNPBluetooth.h"
 #import "RNPNotification.h"
 #import "RNPAudioVideo.h"
-#import "RNPEvent.h"
 #import "RNPPhoto.h"
 #import "RNPContacts.h"
 #import "RNPBackgroundRefresh.h"
@@ -41,7 +39,6 @@
 @interface ReactNativePermissions()
 @property (strong, nonatomic) RNPLocation *locationMgr;
 @property (strong, nonatomic) RNPNotification *notificationMgr;
-@property (strong, nonatomic) RNPBluetooth *bluetoothMgr;
 @end
 
 @implementation ReactNativePermissions
@@ -56,7 +53,7 @@ RCT_EXPORT_MODULE();
 {
     if (self = [super init]) {
     }
-    
+
     return self;
 }
 
@@ -96,9 +93,9 @@ RCT_EXPORT_METHOD(openSettings:(RCTPromiseResolveBlock)resolve rejecter:(RCTProm
 RCT_REMAP_METHOD(getPermissionStatus, getPermissionStatus:(RNPType)type json:(id)json resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
     NSString *status;
-    
+
     switch (type) {
-            
+
         case RNPTypeLocation: {
             NSString *locationPermissionType = [RCTConvert NSString:json];
             status = [RNPLocation getStatusForType:locationPermissionType];
@@ -115,15 +112,6 @@ RCT_REMAP_METHOD(getPermissionStatus, getPermissionStatus:(RNPType)type json:(id
             break;
         case RNPTypeContacts:
             status = [RNPContacts getStatus];
-            break;
-        case RNPTypeEvent:
-            status = [RNPEvent getStatus:@"event"];
-            break;
-        case RNPTypeReminder:
-            status = [RNPEvent getStatus:@"reminder"];
-            break;
-        case RNPTypeBluetooth:
-            status = [RNPBluetooth getStatus];
             break;
         case RNPTypeNotification:
             status = [RNPNotification getStatus];
@@ -144,7 +132,7 @@ RCT_REMAP_METHOD(getPermissionStatus, getPermissionStatus:(RNPType)type json:(id
 RCT_REMAP_METHOD(requestPermission, permissionType:(RNPType)type json:(id)json resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
     NSString *status;
-    
+
     switch (type) {
         case RNPTypeLocation:
             return [self requestLocation:json resolve:resolve];
@@ -156,12 +144,6 @@ RCT_REMAP_METHOD(requestPermission, permissionType:(RNPType)type json:(id)json r
             return [RNPPhoto request:resolve];
         case RNPTypeContacts:
             return [RNPContacts request:resolve];
-        case RNPTypeEvent:
-            return [RNPEvent request:@"event" completionHandler:resolve];
-        case RNPTypeReminder:
-            return [RNPEvent request:@"reminder" completionHandler:resolve];
-        case RNPTypeBluetooth:
-            return [self requestBluetooth:resolve];
         case RNPTypeNotification:
             return [self requestNotification:json resolve:resolve];
         case RNPTypeSpeechRecognition:
@@ -169,7 +151,7 @@ RCT_REMAP_METHOD(requestPermission, permissionType:(RNPType)type json:(id)json r
         default:
             break;
     }
-    
+
 
 }
 
@@ -178,44 +160,36 @@ RCT_REMAP_METHOD(requestPermission, permissionType:(RNPType)type json:(id)json r
     if (self.locationMgr == nil) {
         self.locationMgr = [[RNPLocation alloc] init];
     }
-    
+
     NSString *type = [RCTConvert NSString:json];
-    
+
     [self.locationMgr request:type completionHandler:resolve];
 }
 
 - (void) requestNotification:(id)json resolve:(RCTPromiseResolveBlock)resolve
 {
     NSArray *typeStrings = [RCTConvert NSArray:json];
-    
+
     UIUserNotificationType types;
     if ([typeStrings containsObject:@"alert"])
         types = types | UIUserNotificationTypeAlert;
-    
+
     if ([typeStrings containsObject:@"badge"])
         types = types | UIUserNotificationTypeBadge;
-    
+
     if ([typeStrings containsObject:@"sound"])
         types = types | UIUserNotificationTypeSound;
-    
-    
+
+
     if (self.notificationMgr == nil) {
         self.notificationMgr = [[RNPNotification alloc] init];
     }
-    
+
     [self.notificationMgr request:types completionHandler:resolve];
 
 }
 
 
-- (void) requestBluetooth:(RCTPromiseResolveBlock)resolve
-{
-    if (self.bluetoothMgr == nil) {
-        self.bluetoothMgr = [[RNPBluetooth alloc] init];
-    }
-    
-    [self.bluetoothMgr request:resolve];
-}
 
 
 
